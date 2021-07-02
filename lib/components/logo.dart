9e63616602de81c7bbb10b8e60bb43b2f8e8feed
Julian <julian.rechsteiner@gmail.com>
2021-07-02 11:00:28 -0600

    haul icon added

--- lib/components/logo.dart
+++ lib/components/logo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:haul_assignment/utilities.dart';
 
+import '../presentation/haul_icons_icons.dart';
+
 class HaulLogo extends StatelessWidget {
   const HaulLogo({Key key}) : super(key: key);
 
@@ -13,12 +15,7 @@ class HaulLogo extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(50)),
         color: HaulColors.orange,
       ),
-      child: Center(
-        child: Text(
-          'H',
-          style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
-        ),
-      ),
+      child: Center(child: Icon(HaulIcons.haul, color: Colors.white, size: 60)),
     );
   }
 }
